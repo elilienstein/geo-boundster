40d4a02aade8e3e601c530bdb207f14da7d83620
elilienstein <eric.lilienstein@gmail.com>
2016-07-08 09:27:28 -0700

    Eliminated code defining regions from user defaults. Changed event handling to delegate method instead of gesture recognizer

--- Bounder/ViewController.m
+++ Bounder/ViewController.m
@@ -13,21 +13,18 @@
 
 NSString * const kDefaultLatitude = @"savedUserLatitude";
 NSString * const kDefaultLongitude = @"savedUserLongitude";
-NSString * const kDefaultLatDelta = @"savedUserLatDelta";
-NSString * const kDefaultLongDelta = @"savedUserLongDelta";
-
 
 @interface ViewController () <MKMapViewDelegate>
 
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-@property (nonatomic, strong) UITapGestureRecognizer *tapGestureRecognizer;
 @property (strong, nonatomic) NSString *coordinates;
 
 @property (strong, nonatomic) NSString *NEcoordinates;
 @property (strong, nonatomic) NSString *SWcoordinates;
 @property (nonatomic) MKCoordinateRegion myRegion;
-
+@property (weak, nonatomic) UILabel *NEDisplay;
+@property (weak, nonatomic) UILabel *SWDisplay;
 @end
 
 @implementation ViewController{
@@ -41,17 +38,12 @@ NSString * const kDefaultLongDelta = @"savedUserLongDelta";
    
 NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 
-self.navigationItem.title = @"Position the map... then tap to update coordinates";
+self.navigationItem.title = @"Select quadrant, then export coordinates";
 
 lats = [defaults doubleForKey:kDefaultLatitude];
 longs = [defaults doubleForKey:kDefaultLongitude];
-latDelta = [defaults doubleForKey:kDefaultLatDelta];
-longDelta = [defaults doubleForKey:kDefaultLongDelta];
+
 CLLocationCoordinate2D mycoordinate = CLLocationCoordinate2DMake(lats, longs);
-MKCoordinateSpan mySpan = MKCoordinateSpanMake(latDelta, longDelta);
-
-MKCoordinateRegion myRegion = MKCoordinateRegionMake(mycoordinate, mySpan);
-
 
 
 self.mapView.mapType = MKMapTypeHybrid;
@@ -66,38 +58,28 @@ if
 ((lats != 0.0) && (longs != 0.0)) {
     
     dispatch_once(&centerMapFirstTime, ^{
-        [self.mapView setRegion:myRegion animated:NO];
+        [self.mapView setCenterCoordinate:mycoordinate animated:YES];
+        
         
     });
 }
 
 
-self.tapGestureRecognizer = [[UITapGestureRecognizer alloc]
-                             initWithTarget:self
-                             action:@selector(handleTaps:)];
-
-
-[self.view addGestureRecognizer:self.tapGestureRecognizer];
-
 }
 
 
 
-- (void) handleTaps:(UITapGestureRecognizer*)paramSender{
-    
+- (void) mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
    
-    
-    CGPoint point = [self.tapGestureRecognizer locationInView:self.mapView];
-    
-    CLLocationCoordinate2D tapPoint = [self.mapView convertPoint:point toCoordinateFromView:self.view];
     
 
     //1. Find the corners of the map
-    
+
 
     CGPoint nePoint = CGPointMake(self.mapView.bounds.origin.x + self.mapView.bounds.size.width, self.mapView.bounds.origin.y);
     CGPoint swPoint = CGPointMake((self.mapView.bounds.origin.x), (self.mapView.bounds.origin.y + self.mapView.bounds.size.height));
     
+   
     //2.Transform corner points into lat,lng values
     CLLocationCoordinate2D neCoord = [self.mapView convertPoint:nePoint toCoordinateFromView:self.mapView];
     
@@ -111,9 +93,10 @@ self.tapGestureRecognizer = [[UITapGestureRecognizer alloc]
     NSArray *arrayOfNEStrings = @[neLat, neLong];
     NSArray *arrayOfSWStrings = @[swLat, swLong];
     
-  
+   CLLocationCoordinate2D centerCoord = self.mapView.centerCoordinate;
     
-    [self setUserDefaultsWithLatitude:tapPoint.latitude longitude:tapPoint.longitude latitudeDelta:latDelta longitudeDelta:longDelta];
+    [self setUserDefaultsWithLatitude:centerCoord.latitude longitude:centerCoord.longitude];
+    
    NSString *NEcoordinates = [arrayOfNEStrings componentsJoinedByString:@", "];
    NSString *SWcoordinates = [arrayOfSWStrings componentsJoinedByString:@", "];
     self.coordinates = [NEcoordinates stringByAppendingString:SWcoordinates];
@@ -146,12 +129,10 @@ self.tapGestureRecognizer = [[UITapGestureRecognizer alloc]
  }];
  }
 
--(void)setUserDefaultsWithLatitude:(double)latitude longitude:(double)longitude latitudeDelta:(double)dLatitude longitudeDelta:(double)dLongitude {
+-(void)setUserDefaultsWithLatitude:(double)latitude longitude:(double)longitude {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:[NSNumber numberWithDouble:latitude] forKey:kDefaultLatitude];
     [defaults setObject:[NSNumber numberWithDouble:longitude] forKey:kDefaultLongitude];
-    [defaults setObject:[NSNumber numberWithDouble:dLatitude] forKey:kDefaultLatDelta];
-    [defaults setObject:[NSNumber numberWithDouble:dLongitude] forKey:kDefaultLongDelta];
     
     [defaults synchronize];
 }
